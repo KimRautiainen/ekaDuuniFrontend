c5bdd216762f71142ecf6c807b1906b1ea033ceb
Kim Rautiainen <kim.rautiainen@hotmail.com>
2025-04-01 17:44:17 +0300

    asd

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./SearchBar.css";
+import "./Searchbar.css";
 import { FaSearch } from "react-icons/fa";
 import { MdTune } from "react-icons/md"; // ⬅️ Right side icon
 
