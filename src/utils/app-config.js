6324a637c4ccb36d17eb45bbe3e5f554da2ddfc3
Kim Rautiainen <kim.rautiainen@hotmail.com>
2025-05-04 15:29:29 +0300

    Change to Prod
    changed api urls to production api

--- src/utils/app-config.js
+++ src/utils/app-config.js
@@ -1,8 +1,9 @@
-// Local api URLS
+/* // Local api URLS
 export const apiUrl = 'http://localhost:5000/';
 // Auth url
 export const authUrl = 'http://localhost:5000/auth/';
-
+ */
 
 // Cloud api URLS
-// export const apiUrl = 'https://api.example.com/';
+ export const apiUrl = 'https://ekaduunibackend.onrender.com/';
+ export const authUrl = 'https://ekaduunibackend.onrender.com/auth/';
